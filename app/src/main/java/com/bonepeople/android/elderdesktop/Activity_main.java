fc636ee25ca815c5ba8ece612a045e40fd1ee256
bonepeople <bonepeople@sina.com>
2017-03-07 17:57:03 +0800

    创建工程
    添加gitignore和LICENSE

---
+++ app/src/main/java/com/bonepeople/android/elderdesktop/Activity_main.java
@@ -0,0 +1,13 @@
+package com.bonepeople.android.elderdesktop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity_main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
